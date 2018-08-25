d378446c624a6cf8774dfe339c52d605666c2b25
Nikee Rivera <36245877+Nikee-R@users.noreply.github.com>
2018-08-25 10:56:06 -0400

    Add files via upload
    Connection to database functional.

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ var path = require("path");
 var Sequelize = require ("sequelize");
 var basename = path.basename(module.filename);
 var env = process.env.Node_ENV || "development";
-var config = require(_dirname + "/../config/config.json")[env];
+var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
 // ====================== Functionality ====================== //
@@ -23,12 +23,12 @@ if (config.use_env_variable) {
 }
 
 fs 
-    .readdirSync(_dirname)
+    .readdirSync(__dirname)
     .filter(function(file) {
         return(file.indexOf("." !== 0) && (file !== basename) && (file.slice(-3) === ".js"))
     })
     .forEach(function(file) {
-        var model = sequelize.import(path.join(_dirname, file));
+        var model = sequelize.import(path.join(__dirname, file));
         db[model.name] = model;
     });
 
